2a751c9cd22ca14393412c15f8d7210a3357d009
unknown <lemanifik>
2022-07-24 01:29:51 -0400

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const app =require("express")();
 const PORT=process.env.PORT ||3000;
-app.get("", (req, res)=> {res.send("Hello World");});
+app.get("", (req, res)=> {res.send(index.html);});
 app.listen(PORT,()=>{console.log('App up and port ${PORT}');});
