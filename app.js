686b3444f3a22c6aecd29d5a8a38f9824e3ed4f8
unknown <lemanifik>
2022-07-24 01:34:38 -0400

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const app =require("express")();
 const PORT=process.env.PORT ||3000;
-app.get("", (req, res)=> {res.send(index.html);});
+app.get("", (req, res)=> {res.send('<h5 class="text-primary">ADELE </h5> <h6>someone like you</h6>');});
 app.listen(PORT,()=>{console.log('App up and port ${PORT}');});
